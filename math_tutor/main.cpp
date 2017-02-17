eb07af84550f8c93de123b90ae5b5d9e07d8bf49
Michael Dorst <m@mikefdorst.com>
2017-02-16 18:08:47 -0800

    math_tutor: Add answer checking w/ snarky responses

--- math_tutor/main.cpp
+++ math_tutor/main.cpp
@@ -21,24 +21,57 @@ int main()
     // Assign sum a random number between 200 and 999
     unsigned sum = sum_dist(gen);
     
-    // The addend must be at least 100, and cannot be more than 899,
-    // because the augend must be at least 100 and the sum must be less
-    // than 1000.
-    uniform_int_distribution<> addend_dist(100, 899);
+    // The addend must be at least 100, and cannot be more than
+    // sum - 100, because the augend must be at least 100,
+    // and addend + augend = sum.
+    uniform_int_distribution<> addend_dist(100, sum - 100);
     unsigned addend = addend_dist(gen);
 
-    // The augend must be at least 100, and cannot be more than
-    // 999 - addend.
-    uniform_int_distribution<> augend_dist(100, 999-addend);
-    unsigned augend = augend_dist(gen);
-    
-    cout << "When you are ready to see the answer, press <Enter>..."
-         << endl << endl
+    unsigned augend = sum - addend;
+
+    // Will be used to take user input
+    unsigned input;
+
+    // I want to generate a random snarky response, out of a pool
+    // of snarky responses.
+    // I need a random number to select which response I will use.
+    uniform_int_distribution<> snarky_response_dist(0, 3);
+    size_t snarky_response_index = snarky_response_dist(gen);
+
+    // Here are my pools of snarky responses
+    string snarky_positive_response_pool[] = {
+        "You're a mathematical genius.",
+        "Good job. We're all very impressed.",
+        "Arithmetic ain't got nothin on you.",
+        "Oh bravo. You're an inspiration to us all."
+    };
+    string snarky_negative_response_pool[] = {
+        "Trum says: WRONG!!!",
+        "You need help.",
+        "You forgot to carry the 1, genius.",
+        "Oh COME ON..."
+    };
+
+    cout << endl << endl
          << "   " << addend << endl
          << " + " << augend << endl
-         << "------";
-    cin.get();
+         << "------" << endl;
 
-    cout << "   " <<  addend + augend << endl;
+    cout << "   ";
+    cin >> input;
+
+    if (input != sum)
+    {
+        string response = snarky_negative_response_pool[snarky_response_index];
+        cout << response << endl
+             << endl
+             << "The answer is " << sum << endl;
+    }
+    else
+    {
+        string response = snarky_positive_response_pool[snarky_response_index];
+        cout << endl
+             << response << endl;
+    }    
 }
 
